f3e6f41ee13dbbfd48a9ce200b71e9ede943c5eb
Ben Foster <ben@planetcloud.co.uk>
2013-03-07 10:12:28 +0000

    Automated build/nuget pack/pub

---
+++ src/Fabrik.SimpleBus/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Fabrik.SimpleBus")]
+[assembly: AssemblyDescription("Fabrik.SimpleBus is a simple in-process message bus written in C# that uses TPL Dataflow to provide asynchronous messaging capabilities.")]
--- src/build-nuget.cmd
+++
@@ -1,1 +0,0 @@
-@.nuget\NuGet.exe pack
